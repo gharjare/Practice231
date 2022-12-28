708a63a42139485aac9e9734ab1aa756cdd427e9
gharjare <gharjarechetan@gmail.com>
2022-12-28 16:29:57 +0530

    [Chetan]:performing addition operation

---
+++ Addition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo231
+{
+    public class Addition
+    {
+        public void Add()
+        {
+            int a = 10;
+            int b = 20;
+            int c = a+ b;
+
+            Console.WriteLine(c);
+        }
+    }
+}
